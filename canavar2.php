07e4302f0b767dfe8f56c0780d2be33a06c97174
icayir <haydarucar1@gmail.com>
2025-07-03 14:34:51 +0300

    yuva&canavar2

---
+++ canavar2.php
@@ -0,0 +1,268 @@
+<?php include "header_head.php"; ?>
+    <title>37/b || canavarlar 2</title>
+<?php include "header_nav.php"; ?>
+
+<body>
+<div class="breadcrumbs__section breadcrumbs__section-thin brk-bg-center-cover lazyload" data-bg="d/b.jpg" data-brk-library="component__breadcrumbs_css">
+    <span class="brk-abs-bg-overlay brk-bg-grad opacity-80"></span>
+    <div class="breadcrumbs__wrapper">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-12 col-lg-6">
+                    <div class="d-flex justify-content-lg-end justify-content-start pr-40 pr-xs-0 breadcrumbs__title">
+                        <h2 class="brk-white-font-color font__weight-semibold font__size-48 line__height-68 font__family-montserrat">
+                        canavarlar 2
+                        </h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<section class="pt-60" id="gallery">
+			<div class="container">
+				
+				<div class="brk-gradien-carousel position-relative brk-z-index-10" data-brk-library="component__gallery_sliders,fancybox">
+					
+					<div class="brk-square-dots pb-90">
+						<div class="brk-gradien-carousel__items slick-loading">
+							<div>
+								<div class="brk-gradient-card brk-gradient-card_hover-sizer" data-filter="photography" data-brk-library="component__gallery_sliders_css">
+									<img class="brk-gradient-card__img lazyload" alt="alt" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="u/202.jpg">
+									<div class="brk-gradient-card__overlay brk-base-bg-gradient-6-black"></div>
+									<div class="brk-gradient-card__body">
+										<a href="u/02.jpg" class="brk-gradient-card__img-frame fancybox" data-fancybox="gallery">
+											<i class="fal fa-plus font__size-23 brk-base-font-color"></i>
+										</a>
+									
+									</div>
+								</div>
+							</div>
+							<div>
+								<div class="brk-gradient-card brk-gradient-card_hover-sizer" data-filter="branding" data-brk-library="component__gallery_sliders_css">
+									<img class="brk-gradient-card__img lazyload" alt="alt" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="u/203.jpg">
+									<div class="brk-gradient-card__overlay brk-base-bg-gradient-6-black"></div>
+									<div class="brk-gradient-card__body">
+										<a href="u/03.jpg" class="brk-gradient-card__img-frame fancybox" data-fancybox="gallery">
+											<i class="fal fa-plus font__size-23 brk-base-font-color"></i>
+										</a>
+										
+									</div>
+								</div>
+							</div>
+							<div>
+								<div class="brk-gradient-card brk-gradient-card_hover-sizer" data-filter="branding" data-brk-library="component__gallery_sliders_css">
+									<img class="brk-gradient-card__img lazyload" alt="alt" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="u/204.jpg">
+									<div class="brk-gradient-card__overlay brk-base-bg-gradient-6-black"></div>
+									<div class="brk-gradient-card__body">
+										<a href="u/04.jpg" class="brk-gradient-card__img-frame fancybox" data-fancybox="gallery">
+											<i class="fal fa-plus font__size-23 brk-base-font-color"></i>
+										</a>
+										
+									</div>
+								</div>
+							</div>
+                            <div>
+								<div class="brk-gradient-card brk-gradient-card_hover-sizer" data-filter="branding" data-brk-library="component__gallery_sliders_css">
+									<img class="brk-gradient-card__img lazyload" alt="alt" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="u/205.jpg">
+									<div class="brk-gradient-card__overlay brk-base-bg-gradient-6-black"></div>
+									<div class="brk-gradient-card__body">
+										<a href="u/04.jpg" class="brk-gradient-card__img-frame fancybox" data-fancybox="gallery">
+											<i class="fal fa-plus font__size-23 brk-base-font-color"></i>
+										</a>
+										
+									</div>
+								</div>
+							</div>
+
+                            <div>
+								<div class="brk-gradient-card brk-gradient-card_hover-sizer" data-filter="branding" data-brk-library="component__gallery_sliders_css">
+									<img class="brk-gradient-card__img lazyload" alt="alt" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="u/205.jpg">
+									<div class="brk-gradient-card__overlay brk-base-bg-gradient-6-black"></div>
+									<div class="brk-gradient-card__body">
+										<a href="u/05.jpg" class="brk-gradient-card__img-frame fancybox" data-fancybox="gallery">
+											<i class="fal fa-plus font__size-23 brk-base-font-color"></i>
+										</a>
+										
+									</div>
+								</div>
+							</div>
+                            <div>
+								<div class="brk-gradient-card brk-gradient-card_hover-sizer" data-filter="branding" data-brk-library="component__gallery_sliders_css">
+									<img class="brk-gradient-card__img lazyload" alt="alt" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="u/206.jpg">
+									<div class="brk-gradient-card__overlay brk-base-bg-gradient-6-black"></div>
+									<div class="brk-gradient-card__body">
+										<a href="u/06.jpg" class="brk-gradient-card__img-frame fancybox" data-fancybox="gallery">
+											<i class="fal fa-plus font__size-23 brk-base-font-color"></i>
+										</a>
+										
+									</div>
+								</div>
+							</div>
+                            <div>
+								<div class="brk-gradient-card brk-gradient-card_hover-sizer" data-filter="branding" data-brk-library="component__gallery_sliders_css">
+									<img class="brk-gradient-card__img lazyload" alt="alt" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="u/2207.jpg">
+									<div class="brk-gradient-card__overlay brk-base-bg-gradient-6-black"></div>
+									<div class="brk-gradient-card__body">
+										<a href="u/07.jpg" class="brk-gradient-card__img-frame fancybox" data-fancybox="gallery">
+											<i class="fal fa-plus font__size-23 brk-base-font-color"></i>
+										</a>
+										
+									</div>
+								</div>
+							</div>
+                            
+                             <div>
+								<div class="brk-gradient-card brk-gradient-card_hover-sizer" data-filter="branding" data-brk-library="component__gallery_sliders_css">
+									<img class="brk-gradient-card__img lazyload" alt="alt" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="u/208.jpg">
+									<div class="brk-gradient-card__overlay brk-base-bg-gradient-6-black"></div>
+									<div class="brk-gradient-card__body">
+										<a href="u/08.jpg" class="brk-gradient-card__img-frame fancybox" data-fancybox="gallery">
+											<i class="fal fa-plus font__size-23 brk-base-font-color"></i>
+										</a>
+										
+									</div>
+								</div>
+							</div>
+
+                             <div>
+								<div class="brk-gradient-card brk-gradient-card_hover-sizer" data-filter="branding" data-brk-library="component__gallery_sliders_css">
+									<img class="brk-gradient-card__img lazyload" alt="alt" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="u/209.jpg">
+									<div class="brk-gradient-card__overlay brk-base-bg-gradient-6-black"></div>
+									<div class="brk-gradient-card__body">
+										<a href="u/09.jpg" class="brk-gradient-card__img-frame fancybox" data-fancybox="gallery">
+											<i class="fal fa-plus font__size-23 brk-base-font-color"></i>
+										</a>
+										
+									</div>
+								</div>
+							</div>
+
+                             <div>
+								<div class="brk-gradient-card brk-gradient-card_hover-sizer" data-filter="branding" data-brk-library="component__gallery_sliders_css">
+									<img class="brk-gradient-card__img lazyload" alt="alt" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="u/210.jpg">
+									<div class="brk-gradient-card__overlay brk-base-bg-gradient-6-black"></div>
+									<div class="brk-gradient-card__body">
+										<a href="t/10.jpg" class="brk-gradient-card__img-frame fancybox" data-fancybox="gallery">
+											<i class="fal fa-plus font__size-23 brk-base-font-color"></i>
+										</a>
+										
+									</div>
+								</div>
+							</div>
+
+                             <div>
+								<div class="brk-gradient-card brk-gradient-card_hover-sizer" data-filter="branding" data-brk-library="component__gallery_sliders_css">
+									<img class="brk-gradient-card__img lazyload" alt="alt" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="u/211.jpg">
+									<div class="brk-gradient-card__overlay brk-base-bg-gradient-6-black"></div>
+									<div class="brk-gradient-card__body">
+										<a href="u/11.jpg" class="brk-gradient-card__img-frame fancybox" data-fancybox="gallery">
+											<i class="fal fa-plus font__size-23 brk-base-font-color"></i>
+										</a>
+										
+									</div>
+								</div>
+							</div>
+
+                             <div>
+								<div class="brk-gradient-card brk-gradient-card_hover-sizer" data-filter="branding" data-brk-library="component__gallery_sliders_css">
+									<img class="brk-gradient-card__img lazyload" alt="alt" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="u/215.jpg">
+									<div class="brk-gradient-card__overlay brk-base-bg-gradient-6-black"></div>
+									<div class="brk-gradient-card__body">
+										<a href="u/15.jpg" class="brk-gradient-card__img-frame fancybox" data-fancybox="gallery">
+											<i class="fal fa-plus font__size-23 brk-base-font-color"></i>
+										</a>
+										
+									</div>
+								</div>
+							</div>
+
+                             <div>
+								<div class="brk-gradient-card brk-gradient-card_hover-sizer" data-filter="branding" data-brk-library="component__gallery_sliders_css">
+									<img class="brk-gradient-card__img lazyload" alt="alt" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="u/216.jpg">
+									<div class="brk-gradient-card__overlay brk-base-bg-gradient-6-black"></div>
+									<div class="brk-gradient-card__body">
+										<a href="u/16.jpg" class="brk-gradient-card__img-frame fancybox" data-fancybox="gallery">
+											<i class="fal fa-plus font__size-23 brk-base-font-color"></i>
+										</a>
+										
+									</div>
+								</div>
+							</div>
+
+                             <div>
+								<div class="brk-gradient-card brk-gradient-card_hover-sizer" data-filter="branding" data-brk-library="component__gallery_sliders_css">
+									<img class="brk-gradient-card__img lazyload" alt="alt" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="u/213.jpg">
+									<div class="brk-gradient-card__overlay brk-base-bg-gradient-6-black"></div>
+									<div class="brk-gradient-card__body">
+										<a href="u/13.jpg" class="brk-gradient-card__img-frame fancybox" data-fancybox="gallery">
+											<i class="fal fa-plus font__size-23 brk-base-font-color"></i>
+										</a>
+										
+									</div>
+								</div>
+							</div>
+                            
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+<div class="container mt-40  text-center">
+
+</div>
+
+<div class="container mt-60  text-center">
+    <div class="row">
+        <?php for ($i = 1; $i <= 19; $i++): ?>
+            <div class="col-lg-6 col-md-4 col-sm-6 mb-2">
+                <div class="brk-gallery-card" data-brk-library="component__gallery" style="position: relative; width: 100%; padding-bottom: 0%;">
+                    
+                     
+					<img src="h/web<?php echo $i; ?>.jpg" class=" brk-gallery-card__img lazyload" alt="Image <?php echo $i; ?>" style="width: 100%; height: 100%; object-fit: contain; cursor: pointer;">
+					<span class="brk-gallery-card__overlay-angle"></span>
+					<a href="h/web<?php echo $i; ?>.jpg" data-fancybox="gallery" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></a>	
+					</a>
+					<span class="brk-gallery-card__angle-btn pr-15 pb-15 justify-content-end align-items-end d-flex">
+						<i class="fas fa-search font__size-16 brk-white-font-color" aria-hidden="true"></i>
+					</span>
+                </div>
+            </div>
+        <?php endfor; ?>
+    </div>
+</div>
+
+						
+
+<?php include "footer.php"; ?>
+
+<!-- Fancybox CSS and JS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css">
+<script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.umd.js"></script>
+<!-- Bootstrap JS, Popper.js -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+<script>
+    // Initialize Fancybox
+    Fancybox.bind('[data-fancybox="gallery"]', {
+        Toolbar: {
+            display: [
+                { id: "prev", position: "left" },
+                { id: "counter", position: "center" },
+                { id: "next", position: "right" },
+                { id: "zoom", position: "right" },
+                { id: "close", position: "right" }
+            ]
+        },
+        Image: {
+            zoom: true
+        }
+    });
+</script>
+</body>
--- galeri.php
+++ galeri.php
@@ -85,8 +85,58 @@
 					</div>
 				</div>
 				
-					
-					
+				</div>
+			</div>
+			
+		</section>
+
+		<section id="portfolio2">
+			<div class="brk-portfolio-galleries">
+				<div class="row brk-gutters-5">
+					<div class="col-12 col-md-6">
+						<div class="brk-portfolio-galleries__card" data-brk-library="component__portfolio_galleries">
+							<img class="brk-abs-img lazyload" alt="alt" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="h/web1.jpg">
+							<div class="brk-portfolio-galleries__label pt-60 pl-70 pr-60 pb-50">
+								<h2 class="font__family-roboto font__size-56 font__weight-thin mb-2">canavarlar 2 </h2>
+								<h3 class="brk-dark-font-color font__family-montserrat font__size-12 font__weight-normal text-uppercase letter-spacing-100 mb-auto">burada onlar yaşıyorlardı, Canavarlar  </h3>
+								<div class="brk-social-links brk-black-font-color font__size-13 brk-social-links_opacity" data-brk-library="component__social_links">
+								<a href="https://www.instagram.com/37bsanatyeri/" class="brk-social-links__item wow fadeInUp" data-wow-delay="1.1s" data-wow-duration=".35s">
+									<i class="fab fa-instagram"></i>
+								</a>
+								
+								<a href="https://www.youtube.com/channel/UC7-kJxAMHTYhenhGtfz7_Mw" class="brk-social-links__item wow fadeInUp" data-wow-delay="1.3s" data-wow-duration=".35s">
+									<i class="fab fa-youtube"></i>
+								</a>
+								
+								</div>
+								<a href="canavar2.php" class="text-uppercase font__family-montserrat font__size-12 font__weight-bold text-underline letter-spacing-100 mt-30"><u>daha fazla</u></a>
+							</div>
+							
+						</div>
+					</div>
+					<div class="col-12 col-md-6">
+						<div class="brk-portfolio-galleries__card" data-brk-library="component__portfolio_galleries">
+							<img class="brk-abs-img lazyload" alt="alt" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="g/web1.jpg">
+							<div class="brk-portfolio-galleries__label pt-60 pl-70 pr-60 pb-50">
+								<h2 class="font__family-roboto font__size-56 font__weight-thin mb-2">yuva </h2>
+								<div class="brk-social-links brk-black-font-color font__size-13 brk-social-links_opacity" data-brk-library="component__social_links">
+								<a href="https://www.instagram.com/37bsanatyeri/" class="brk-social-links__item wow fadeInUp" data-wow-delay="1.1s" data-wow-duration=".35s">
+									<i class="fab fa-instagram"></i>
+								</a>
+								
+								<a href="https://www.youtube.com/channel/UC7-kJxAMHTYhenhGtfz7_Mw" class="brk-social-links__item wow fadeInUp" data-wow-delay="1.3s" data-wow-duration=".35s">
+									<i class="fab fa-youtube"></i>
+								</a>
+								
+								</div>
+								<a href="yuva.php" class="text-uppercase font__family-montserrat font__size-12 font__weight-bold text-underline letter-spacing-100 mt-30"><u>daha fazla</u></a>
+							</div>
+						</div>
+						</div>
+						
+					</div>
+				</div>
+				
 				</div>
 			</div>
 			
